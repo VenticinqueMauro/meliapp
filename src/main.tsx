d7d1fd73815088dd18bce7182a52763ea39aefda
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-03-15 19:25:39 -0300

    problemas con vercel

--- src/main.tsx
+++ src/main.tsx
@@ -11,16 +11,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage'
 
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
-// };
-
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyAixUNS-9F8ebjYRHD1RYVShnH4_JumG20",
