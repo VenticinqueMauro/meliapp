19cd30ccbac89b3533aa20571cb0fa0fcc84f6ac
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-03-17 13:54:24 -0300

    agregado de variables de entorno

--- src/main.tsx
+++ src/main.tsx
@@ -12,13 +12,22 @@ import { getStorage } from 'firebase/storage'
 
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAixUNS-9F8ebjYRHD1RYVShnH4_JumG20",
+//   authDomain: "lacarta-92bf7.firebaseapp.com",
+//   projectId: "lacarta-92bf7",
+//   storageBucket: "lacarta-92bf7.appspot.com",
+//   messagingSenderId: "902477021868",
+//   appId: "1:902477021868:web:c4a5be3d280454c0655509"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAixUNS-9F8ebjYRHD1RYVShnH4_JumG20",
-  authDomain: "lacarta-92bf7.firebaseapp.com",
-  projectId: "lacarta-92bf7",
-  storageBucket: "lacarta-92bf7.appspot.com",
-  messagingSenderId: "902477021868",
-  appId: "1:902477021868:web:c4a5be3d280454c0655509"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
